9cae6870a964ebccd9305d92c86cd2869d6ec012
TheValeHack <thevale145@gmail.com>
2025-12-24 20:23:17 +0700

    feat: change page max width

--- src/app/(paket)/paket_dewekan/page.js
+++ src/app/(paket)/paket_dewekan/page.js
@@ -84,7 +84,7 @@ export default function PaketDewekan() {
   return (
     <div className="w-full overflow-hidden">
       <Navbar className={"navbar"} session={session} />
-      <div className="max-w-[1950px] mx-auto flex flex-col w-screen relative">
+      <div className="max-w-[2160px] mx-auto flex flex-col w-screen relative">
         <section
           id="paket"
           className="w-full min-h-[75vh] py-32 flex flex-col justify-start items-center relative"
